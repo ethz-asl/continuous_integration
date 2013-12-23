7de26a74a830e85906a6855c2cbcdbe753d875d3
Simon Lynen <simon.lynen@mavt.ethz.ch>
2013-12-23 02:00:12 +0100

    split up scripts

--- run_build.sh
+++ run_build.sh
@@ -1,51 +1,11 @@
 #!/bin/bash
 CWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Reload ci scripts from repo.
 cd $DIR
 git pull
 cd $CWD
 
-PACKAGE="--all"
-DEPENDENCIES=""
-
-for i in "$@"
-do
-case $i in
-    -p=*|--packages=*)
-    PACKAGES="${i#*=}"
-    ;;
-    -d=*|--dependencies=*)
-    DEPENDENCIES="${i#*=}"
-    ;;
-    *)
-       echo "Usage: run_build [{-d|--dependencies}=dependency.git] [{-p|--packages}=packages]"
-    ;;
-esac
-done
-echo PACKAGES = "${PACKAGES}"
-echo DEPENDENCIES = "${DEPENDENCIES}"
-
-CPPCHECK_PARAMS=". --enable=all "
-
-mkdir -p $WORKSPACE/src && cd $WORKSPACE/src
-for dependencies in ${DEPENDENCIES}
-do
-    foldername_w_ext=${dependencies##*/}
-    foldername=${foldername_w_ext%.*}
-    if [ -d $foldername ]; then
-      echo Folder "$foldername" exists, running git pull on "$dependencies"
-      cd "$foldername" && git pull && cd ..
-    else
-      echo Folder "$foldername" does not exists, running git clone "$dependencies"
-      git clone "$dependencies" --recursive
-    fi  
-    CPPCHECK_PARAMS="$CPPCHECK_PARAMS -i$foldername"
-done
-cd $WORKSPACE
-
-$DIR/run_build_catkin_or_rosbuild ${PACKAGES}
-
-echo "Running cppcheck $CPPCHECK_PARAMS ..."
-# Run cppcheck excluding dependencies.
-cd $CWD
-cppcheck $CPPCHECK_PARAMS > cppcheck-result.xml
+# Now run actual script.
+run_build_impl.sh
---
+++ run_build_impl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+CWD=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PACKAGE="--all"
+DEPENDENCIES=""
+
+# Download / update dependencies.
+for i in "$@"
+do
+case $i in
+    -p=*|--packages=*)
+    PACKAGES="${i#*=}"
+    ;;
+    -d=*|--dependencies=*)
+    DEPENDENCIES="${i#*=}"
+    ;;
+    *)
+       echo "Usage: run_build [{-d|--dependencies}=dependency.git] [{-p|--packages}=packages]"
+    ;;
+esac
+done
+echo PACKAGES = "${PACKAGES}"
+echo DEPENDENCIES = "${DEPENDENCIES}"
+
+# Prepare cppcheck ignore list. We want to skip dependencies.
+CPPCHECK_PARAMS=". --enable=all "
+
+mkdir -p $WORKSPACE/src && cd $WORKSPACE/src
+for dependencies in ${DEPENDENCIES}
+do
+    foldername_w_ext=${dependencies##*/}
+    foldername=${foldername_w_ext%.*}
+    if [ -d $foldername ]; then
+      echo Folder "$foldername" exists, running git pull on "$dependencies"
+      cd "$foldername" && git pull && cd ..
+    else
+      echo Folder "$foldername" does not exists, running git clone "$dependencies"
+      git clone "$dependencies" --recursive
+    fi  
+    CPPCHECK_PARAMS="$CPPCHECK_PARAMS -i$foldername"
+done
+cd $WORKSPACE
+
+#Now run the build.
+$DIR/run_build_catkin_or_rosbuild ${PACKAGES}
+
+echo "Running cppcheck $CPPCHECK_PARAMS ..."
+# Run cppcheck excluding dependencies.
+cd $CWD
+cppcheck $CPPCHECK_PARAMS > cppcheck-result.xml
