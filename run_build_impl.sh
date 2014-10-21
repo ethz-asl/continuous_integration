f4bfd0b23d488cec2a4bbfa474631b07930f59f4
Paul Furgale <paul.furgale@gmail.com>
2014-10-21 19:18:39 +0200

    Typo

--- run_build_impl.sh
+++ run_build_impl.sh
@@ -91,7 +91,7 @@ do
       echo Package "$foldername" exists, running git fetch --depth 1, git reset --hard origin/HEAD and git submodule update --recursive on "$dependency"
       cd "$foldername" && git fetch --depth 1 && git checkout origin/${branch} && git submodule update --recursive && cd ..
     else
-      echo Package "$foldername" does not exists, running git clone "$dependency" --recursive
+      echo Package "$foldername" does not exist, running git clone "$dependency" --recursive
       git clone -b ${branch} "$dependency" --recursive --depth 1 --single-branch
     fi
 done
