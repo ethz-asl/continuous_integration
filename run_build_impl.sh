fc537b711d16cb42104c5eab12e53d18d461d226
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2015-05-08 16:44:44 +0200

    Update run_build_impl.sh

--- run_build_impl.sh
+++ run_build_impl.sh
@@ -138,8 +138,8 @@ then
 	# Make a separate workspace for the deps, so we can exclude them from cppcheck etc.
 	mkdir -p $WORKSPACE/$DEPS
 	cd $WORKSPACE/$DEPS	
-	echo "- git: {local-name: $DEPS/aslam_install, uri: 'git@github.com:ethz-asl/aslam_install.git'}" | wstool  merge -t $WORKSPACE/src -
-	echo "- git: {local-name: $DEPS/catkin_simple, uri: '${CATKIN_SIMPLE_URL}'}" | wstool  merge -t $WORKSPACE/src -
+	echo "- git: {local-name: $WORKSPACE/$DEPS/aslam_install, uri: 'git@github.com:ethz-asl/aslam_install.git'}" | wstool  merge -t $WORKSPACE/src -
+	echo "- git: {local-name: $WORKSPACE/$DEPS/catkin_simple, uri: '${CATKIN_SIMPLE_URL}'}" | wstool  merge -t $WORKSPACE/src -
 	wstool update -t $WORKSPACE/src -j8
 
     echo "Dependencies specified by rosinstall file.";
