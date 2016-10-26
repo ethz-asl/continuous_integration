e281b56cb8b2fa4989508e5cd290d056b60ea02a
HannesSommer <hannes.sommer@mavt.ethz.ch>
2016-10-26 13:34:44 +0200

    Test DEBIAN_FRONTEND==noninteractive

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 echo "Hello this is the prepare script of continuous_integration";
 
+if [[ "$DEBIAN_FRONTEND" != noninteractive ]] ;then
+  echo "DEBIAN_FRONTEND=$DEBIAN_FRONTEND instead of noninteractive!" >&2
+  exit -1
+fi
+
 sudo apt-get install git
