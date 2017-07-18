fea97d2d5c92b430305dcea4de8715e81541caaa
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-07-18 09:59:22 +0200

    Make run_build space tolerant

--- run_build.sh
+++ run_build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 ## This script does nothing but update the local repo and then forward everything
 ## to the actual build script which by this time is updated from the remote.
-CWD=$(pwd)
+CWD="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Reload ci scripts from repo.
-cd $DIR
+cd "$DIR"
 git checkout master
 git pull
-cd $CWD
+cd "$CWD"
 
 # Now run actual script.
 $DIR/run_build_impl.sh "$@"
