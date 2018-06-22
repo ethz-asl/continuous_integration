874944d7cf5bf615ef92b0c177cdf3346208ba6a
HannesSommer <hannes.sommer@mavt.ethz.ch>
2018-06-22 12:38:36 +0200

    Install rsync
    It is a dependency for the unit tests

--- prepare-build.sh
+++ prepare-build.sh
@@ -7,7 +7,7 @@ if [[ "$DEBIAN_FRONTEND" != noninteractive ]] ;then
 fi
 
 if [[ $(uname) == "Linux" ]]; then
-  sudo apt-get install -y git imagemagick
+  sudo apt-get install -y git imagemagick rsync
 else
   echo "Platform $(uname) is not supported! Go away!"
 fi
