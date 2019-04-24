a723836678e7ce004781cd543ec77c72cc85380e
Fadri Furrer <ffurrer@gmail.com>
2019-04-24 15:29:18 +0200

    added a function to compare if repos only differ in ssh and https

--- modules/rosinstall_diff.py
+++ modules/rosinstall_diff.py
@@ -56,6 +56,26 @@ class Entry():
   def __str__(self):
     return self.local_name + "(%s, %s)" % (self.uri, self.version)
 
+  def compare_ssh_vs_https(self, other):
+    if isinstance(other, self.__class__):
+      normalized_self_uri = normalizeGithubUrls(self.uri)
+      normalized_other_uri = normalizeGithubUrls(other.uri)
+      are_same = normalized_self_uri.split("github.com")[1][1:] == normalized_other_uri.split("github.com")[1][1:]
+      ssh_entry = ""
+      https_entry = ""
+      if self.uri.split("github.com")[0] == "git@" and other.uri.split("github.com")[0] == "https://":
+        ssh_entry = self.uri
+        https_entry = other.uri
+      elif other.uri.split("github.com")[0] == "git@" and self.uri.split("github.com")[0] == "https://":
+        ssh_entry = other.uri
+        https_entry = self.uri
+      else:
+        print("ERROR: One of the entries of {} is not a valid github repo. Local repos are not supported here. First uri: {}, \nsecond uri: {}".format(self.local_name, self.uri, other.uri))
+        return sys.exit(-1)
+      return ssh_entry, https_entry, are_same
+    else:
+      return NotImplemented
+
 
 if __name__ == "__main__":
   baseFile = sys.argv[1]
@@ -70,7 +90,7 @@ if __name__ == "__main__":
     from os import listdir
     from os.path import join
     if not isdir(updateFile):
-      print ("ERROR : %s is no directory)" % (updateFile), file=sys.stderr)
+      print("ERROR : %s is no directory)" % (updateFile), file=sys.stderr)
       sys.exit(3)
 
     for f in listdir(updateFile):
@@ -84,10 +104,14 @@ if __name__ == "__main__":
       if ue.getId() in baseMap:
         old = baseMap[ue.getId()]
         if not old == ue:
-          print ("ERROR : %s : %s differs from former %s : %s (different versions / URIs for the same local-name are not supported)" % (updateFile, ue, baseFile, old), file = sys.stderr)
-          errorCout+=1
+          ssh_entry, https_entry, same_but_ssh_vs_https = old.compare_ssh_vs_https(ue)
+          if same_but_ssh_vs_https:
+            print("WARNING: {} : {} differs from former {} : {} (using {})".format(updateFile, ue, baseFile, old, old))
+          else:
+            print("ERROR : %s : %s differs from former %s : %s (different versions / URIs for the same local-name are not supported)" % (updateFile, ue, baseFile, old), file = sys.stderr)
+            errorCout += 1
       else:
         print(ue.getId())
-  
+
   if errorCout:
     sys.exit(-1)
